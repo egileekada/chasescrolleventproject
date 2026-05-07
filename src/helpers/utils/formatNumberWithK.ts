e295ef5ddd1e5c10ba831a8977c9df61981e4095
egileekada <egileoniso.ekada@gmail.com>
2026-05-07 14:34:19 +0100

    Update formatNumberWithK function to use 'm' for million instead of 'million' for improved brevity

--- src/helpers/utils/formatNumberWithK.ts
+++ src/helpers/utils/formatNumberWithK.ts
@@ -3,7 +3,7 @@ export const formatNumberWithK = (number: any, symbol?: boolean, prefix = "₦")
     if(number === 0 || !number) {
         return "0"
     } else {
-        return (symbol ? (prefix) : "")+(number > 999999 ? `${Math.trunc(number / 1000000)}million` : number > 999 ? `${Math.trunc(number / 1000)}k` : number)
+        return (symbol ? (prefix) : "")+(number > 999999 ? `${Math.trunc(number / 1000000)}m` : number > 999 ? `${Math.trunc(number / 1000)}k` : number)
     }
 }
 
